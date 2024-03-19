ee4b0a621e0e11c6aa87cb0409dfdfe7fbfd3fc9
victormuteithia <muteithiavictor@gmail.com>
2024-03-19 17:32:06 +0300

    Completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,45 @@
 #!/usr/bin/env python3
 
+
 class MyString:
-  pass
+    
+    def __init__(self, value = ""):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, value):
+        if isinstance(value, str):
+            self._value = value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        if self.value.endswith(".") == True:
+            return True
+        else:
+            return False
+        
+    def is_question(self):
+        if self.value.endswith("?") == True:
+            return True
+        else:
+            return False
+        
+    def is_exclamation(self):
+        if self.value.endswith("!") == True:
+            return True
+        else:
+            return False
+    
+    def count_sentences(self):
+        split_list = self.value.replace("!", ".").replace("?", ".").split(".")
+        item_to_remove = split_list.count('')
+        for i in range(item_to_remove):
+            split_list.remove('')
+        return len(split_list)
+    
+    
